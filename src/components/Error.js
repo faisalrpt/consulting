34fa1b2e847074ef4c4ba4a37b37aea2c7e21f2e
Mohd_Faisal <mfaisalrpt@gmail.com>
2021-02-09 13:27:14 +0530

    design error page

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,7 +1,14 @@
 const Error = () =>{
     return(
         <>
-        Error component
+        <section className="error">
+            <div className="conatiner">
+                <div className="content">
+                <h1 className="text-center">404</h1>
+                <h2 className="text-center"><span>OOPS ! </span> Page Not Found</h2>
+                </div>
+            </div>
+        </section>
         </>
     )
 }
